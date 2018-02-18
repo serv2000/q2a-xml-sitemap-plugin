c7f2c9192e74bcaaf612a14d66669add7851aa28
abdullah shalaan <abdullah.shalaan@gmail.com>
2018-02-18 17:54:04 -0500

    developer credit added

--- xml-sitemap/qa-xml-sitemap.php
+++ xml-sitemap/qa-xml-sitemap.php
@@ -4,24 +4,9 @@
  * abdullah.shalaan@gmail.com  
  * 
  * forked from the origin repo
-	Question2Answer by Gideon Greenspan and contributors
 	http://www.question2answer.org/
 
-	File: qa-plugin/xml-sitemap/qa-xml-sitemap.php
-	Description: Page module class for XML sitemap plugin
 
-
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	More about this license: http://www.question2answer.org/license.php
 */
 
 class qa_xml_sitemap
